9f2904fec4e04b422f47010fc7480480f2826c7c
Dipak <dipaklean+github@gmail.com>
2023-11-20 15:54:39 +1100

    increased timeout on end to end test

--- crc-sam/tests/end-to-end-test/index.js
+++ crc-sam/tests/end-to-end-test/index.js
@@ -1,11 +1,11 @@
 const puppeteer = require('puppeteer');
-const sleep = async (r) => await new Promise(r => setTimeout(r, 3000));
+const sleep = async (r) => await new Promise(r => setTimeout(r, 5000));
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://resume.dleantech.com');
-  await sleep(3000)
+  await sleep(5000)
   await page.waitForSelector('.visitor-counter');
 
   const counterValue = await page.$eval('.visitor-counter', (counter) => counter.textContent);
